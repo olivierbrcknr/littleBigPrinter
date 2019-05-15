99d3fc626544e7d5b310c1f288e94dea23e695bc
Olivier Brückner <25103920+olivierbrcknr@users.noreply.github.com>
2019-05-15 18:32:46 +0200

    Added matrix animation fns

--- index.js
+++ index.js
@@ -3,11 +3,10 @@ const SerialPort = require('serialport');
 const Printer = require('thermalprinter');
 
 // Modules Hardware
-const five = require('johnny-five');
+const five  = require('johnny-five');
 const Raspi = require('raspi-io').RaspiIO;
 
 const icon = require('./matrix-icons');
-
 
 // ——————————————————————————————————————————
 
@@ -18,8 +17,9 @@ const board = new five.Board({
 let sp = new SerialPort('/dev/ttyUSB0', {
     baudRate: 19200
 });
-let printer = null;
 
+let printer = null;
+let matrix = null;
 
 function resetPrinterSettings(){
     printer
@@ -29,75 +29,140 @@ function resetPrinterSettings(){
         .left();
 }
 
+/*
+//Printer commands
 
- 
-var path = __dirname + '/images/nodebot.png';
+ var path = __dirname + '/images/nodebot.png';
 
+.indent(10)
+.horizontalLine(16)
+.bold(true)
+.indent(10)
+.printLine('first line')
+.bold(false)
+.inverse(true)
+.big(true)
+.right()
+.printLine('second line')
+.printImage(path)
+.print(function() {
+    console.log('Init Done');
+    resetPrinterSettings();
+});
+
+*/
 
 
 
 // init Printer
 sp.on('open',function() {
+
+    console.log('Serialport ready 🔌');
+    
     printer = new Printer(sp);
+
     printer.on('ready', function() {
-        printer
-            .indent(10)
-            .horizontalLine(16)
-            .bold(true)
-            .indent(10)
-            .printLine('first line')
-            .bold(false)
-            .inverse(true)
-            .big(true)
-            .right()
-            .printLine('second line')
-            .printImage(path)
-            .print(function() {
-                console.log('Init Done');
-                resetPrinterSettings();
-            });
+        console.log('Printer ready 🖨');
+        resetPrinterSettings();
+
+        printer.printLine('LittlePrinter is ready!').print(function() {
+            resetPrinterSettings();
+        });
     });
 });
 
-
+// init Johnny-five
 board.on('ready', () => {
 
-    console.log('Board Ready 🎛')
+    console.log('J5 Ready 🤖');
 
 
     // Setup hardware
-    let matrix = new five.Led.Matrix({
+    matrix = new five.Led.Matrix({
         addresses: [0x70],
         controller: "HT16K33",
-        rotation: 3,
+        rotation: 1
     });
 
     let btn = new five.Button("P1-11");
 
     // Run code
-
     matrix.clear();
-
-    btn.on("down", function() {
-        matrix.draw(icon.heart);
-        printer
-            .bold(true)
-            .indent(10)
-            .printLine('Button Pressed')
-            .print(function() {
-                setTimeout(function(){
-                    matrix.clear();
-                },1000);
-            });
-        console.log('down')
-    });
+    pulse( icon.check , 1 , 0.5 );
 
 });
 
 
+// Matrix functions
+function sinusAnim( icon , duration ) {
 
+    const time = 150;
 
+    let frames = 10;
+    if( duration ){ frames = duration / time }
 
+    let frame = 0;
+    matrix.clear();
 
+    let prevIcon = icon;
 
+    let anim = setInterval(nextFrame, time);
+    
+    function nextFrame() {
+        if (frame >= frames) {
+            clearInterval(anim);
+            matrix.clear();
+            frame = 0;
+        } else {
+            frame++;
+            let dummy = [];
+            let i = 0;
+            prevIcon.forEach(function(string){
+               dummy[i] = string.substring(1, 8) + string.substring(0);
+               i++;
+            });
+            prevIcon = dummy;
+            matrix.draw(prevIcon);
+        }
+    }
+}
 
+function pulse( icon , pulses , speed ) {
+
+    const step = 10 * speed;
+    let frames = pulses * (100 / step)*2;
+    let time = 200;
+
+    let anim = setInterval(nextFrame, time);
+
+    let brightness = 0;
+    let direction = 1
+    let frame = 0;
+
+    matrix.clear();
+
+    function nextFrame() {
+        if (frame >= frames) {
+            clearInterval(anim);
+            matrix.clear();
+            frame = 0;
+            matrix.brightness(100);
+        } else {
+            frame++;
+            matrix.brightness(brightness);
+            matrix.draw(icon);
+
+            if( direction == 1 && brightness >= 100 ){
+                direction = 0;
+            }
+            if( direction == 0 && brightness <= 0 ){
+                direction = 1;
+            }
+            if(direction){
+                brightness = brightness + step;
+            }else{
+                brightness = brightness - step
+            }
+        }
+    }
+}
